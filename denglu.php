64bf85dc652d3ef439c0fabb8a7dcdfbf38e0f7f
zheng yong <25812739@qq.com>
2018-10-15 13:37:40 -0300

    Update denglu.php

--- denglu.php
+++ denglu.php
@@ -1,61 +1,73 @@
+<?php  session_start () ;
+        if (isset ($_SESSION['xpttcomchaxun'])) {
+            echo "<script language=javascript>window.location='shujuku.php'</script>";
+            exit () ;
+        }
+        ?>
+
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<!-- 欢迎使用，程序来自：xptt.com  有什么建议欢迎交流 -->
-<meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-<title>xptt通用查询系统-支持后台创建txt数据库</title>
-<style>
-    .chax{width:550px;margin:0 auto;}
-  body,input{font:menu}
-@media only screen and (max-width: 750px)  {
-    .chax {
-        width: 100%;
-    }
-}
+    <!-- 欢迎使用，程序来自：xptt.com  有什么建议欢迎交流 -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>xptt通用查询系统-支持后台创建txt数据库</title>
+    <style>
+        .chax {
+            width:550px;
+            margin:0 auto;
+        }
+        body,input {
+            font:menu
+        }
+@media only screen and (max-width: 750px) {
+            .chax {
+                width: 100%;
+            }
+        }
 
-@media only screen and (max-width: 470px)  {
-    .chax  {
-        width: 100%;
-    }
-}
-    
-    @media only screen and (max-width: 334px)  {
-    .chax  {
-        width: 100%;
-    }
-}
+@media only screen and (max-width: 470px) {
+            .chax {
+                width: 100%;
+            }
+        }
+
+@media only screen and (max-width: 334px) {
+            .chax {
+                width: 100%;
+            }
+        }
 
 
 
-</style>
-<div class="chax">
-<form  name="form" method="post" action="shujuku.php" style=" margin-top:5px;">
- <table width="100%" border="0" align="center" cellpadding="5" cellspacing="1" bgcolor="#B3B3B3">
-  <tr>
-    <td colspan="3" align="center" bgcolor="#EBEBEB" class="font">xptt通用查询系统-支持后台创建txt数据库</td>
-    </tr>
-  <tr>
-    <td width="89" align="center" bgcolor="#FFFFFF" class="font">管理密码:</td>
-    <td colspan="2" bgcolor="#FFFFFF" class="font">
-      
-    
-    
-    <span id=box><input type="password" name="password" id="password" size="16" ></span>
-    </td>
-    </tr>
-    <tr>
-      <td width="89" align="center" bgcolor="#FFFFFF" class="font"><a href="index.php">返回首页</a></td>
-    <td colspan="3" align="left" valign="top" bgcolor="#FFFFFF" class="font">
-     <input type="submit" name="submitjizhang" value="登录" /></td>
-    </tr>
-</table>
- <table width="100" border="0" align="center" cellpadding="0" cellspacing="0">
-   <tr>
-     <td height="5"></td> 
-   </tr>
- </table>
+    </style>
+    <div class="chax">
+        <form name="form" method="post" action="shujuku.php" style=" margin-top:5px;">
+            <table width="100%" border="0" align="center" cellpadding="5" cellspacing="1" bgcolor="#B3B3B3">
+                <tr>
+                    <td colspan="3" align="center" bgcolor="#EBEBEB" class="font">xptt通用查询系统-支持后台创建txt数据库</td>
+                </tr>
+                <tr>
+                    <td width="89" align="center" bgcolor="#FFFFFF" class="font">管理密码:</td>
+                    <td colspan="2" bgcolor="#FFFFFF" class="font">
 
-</form>
-</div>
+
+
+                        <span id=box><input type="password" name="password" id="password" size="16"></span>
+                    </td>
+                </tr>
+                <tr>
+                    <td width="89" align="center" bgcolor="#FFFFFF" class="font"><a href="index.php">返回首页</a></td>
+                    <td colspan="3" align="left" valign="top" bgcolor="#FFFFFF" class="font">
+                        <input type="submit" name="submitdenglu" value="登录" /></td>
+                </tr>
+            </table>
+            <table width="100" border="0" align="center" cellpadding="0" cellspacing="0">
+                <tr>
+                    <td height="5"></td>
+                </tr>
+            </table>
+
+        </form>
+    </div>
 </head>
 </html>
